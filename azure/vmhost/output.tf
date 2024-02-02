18e533c489f0a6432cfba580b43928f8ce64d593
Kris Thompson <kris@austinvoicedata.com>
2024-02-02 01:55:23 +0000

    passing pubkey1 from pipe

--- azure/vmhost/output.tf
+++ azure/vmhost/output.tf
@@ -1,9 +1,6 @@
-output "vm0_ipaddress" {
+output "vm_ipaddress" {
     value = azurerm_public_ip.outsideip.ip_address
 }
-output "vm0_fqdn" {
+output "vm_fqdn" {
     value = azurerm_public_ip.outsideip.fqdn
-}
-output "pubkey1" {
-    value = file("${path.module}/pubkeys/${var.pubkey1_file}")
 }
